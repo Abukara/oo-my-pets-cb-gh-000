ae2085e9356dbedb89f51dee0973b5c1bf11a163
Lukas F <lukas.forst48@googlemail.com>
2018-09-22 13:46:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,26 +3,26 @@ require 'pry'
 class Owner
   # code goes here
   attr_accessor :pets, :name
-  
+
   attr_reader :species
-  
+
   @@all = []
-  
+
   def initialize(species="")
-    @species = species  
+    @species = species
     @pets = { :fishes=>[], :dogs=>[], :cats=>[] }
-    
+
     @@all << self
   end
 
   def self.all
     @@all
-  end 
-  
+  end
+
   def self.count
     @@all.length
-  end 
-  
+  end
+
   def self.reset_all
     @@all = []
   end
@@ -30,43 +30,43 @@ class Owner
   def say_species
     "I am a #{@species}."
   end
-  
+
   def buy_fish(name)
     fish = Fish.new(name)
     @pets[:fishes] << fish
-  end 
-  
+  end
+
   def buy_cat(name)
     cat = Cat.new(name)
     @pets[:cats] << cat
   end
-  
+
   def buy_dog(name)
     dog = Dog.new(name)
     @pets[:dogs] << dog
   end
-  
+
   def walk_dogs
     @pets[:dogs].each {|dog| dog.mood = "happy"}
-  end 
-  
+  end
+
   def play_with_cats
     @pets[:cats].each {|cat| cat.mood = "happy"}
-  end 
-  
+  end
+
   def feed_fish
     @pets[:fishes].each {|fish| fish.mood = "happy"}
   end
-  
+
   def sell_pets
     @pets[:dogs].each {|dog| dog.mood = "nervous"}
     @pets[:cats].each {|cat| cat.mood = "nervous"}
     @pets[:fishes].each {|fish| fish.mood = "nervous"}
     @pets = { :fishes=>[], :dogs=>[], :cats=>[] }
-  end 
-  
+  end
+
   def list_pets
     "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
-  end 
-  
+  end
+
 end
